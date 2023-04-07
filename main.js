09d5cb0de33585f6d7482996e8dfd836cd86bb75
andreinikolabai <andreiplus2000@gmail.com>
2023-04-07 18:12:42 +0300

    change

--- main.js
+++ main.js
@@ -3,18 +3,18 @@ const prevButton = sliderContent.querySelector('.prev');
 const nextButton = sliderContent.querySelector('.next');
 const sliderImage = sliderContent.querySelector('.image');
 
-const imageName = ['images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg'];
+const imageNames = ['images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg'];
 let currentImageIndex = 0;
 
 function changeImage(index) {
-    sliderImage.setAttribute('src', imageName[index]);
+    sliderImage.setAttribute('src', imageNames[index]);
     if (index === 0) {
         prevButton.style.display = 'none';
     } else {
         prevButton.style.display = 'block';
     }
 
-    if (index === imageName.length - 1) {
+    if (index === imageNames.length - 1) {
         nextButton.style.display = 'none';
     } else {
         nextButton.style.display = 'block';
